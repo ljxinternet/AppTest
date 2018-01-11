53b6aa1607d8842730d87590d0e2cd630919c577
Junle <ljx5790@hotmail.com>
2018-01-11 13:11:49 +0800

    添加项目文件。

---
+++ App_Test/Activitys/ActivityTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Net.Wifi;
+using Android.Views;
+using Android.Widget;
+
+namespace App_Test.Activitys
+{
+    [Activity(Label = "测试界面")]
+    public class ActivityTest : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            //跟换主题
+            this.SetTheme(Android.Resource.Style.ThemeLight);
+
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.layoutTest);
+        }
+    }
+}
---
+++ App_Test/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace App_Test
+{
+    [Activity(Label = "测试APP", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            //跟换主题
+            this.SetTheme(Android.Resource.Style.ThemeLight);
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+
+            //
+            var btnTest = FindViewById<Button>(Resource.Id.BtnTest);
+            btnTest.Click += (sender, e) =>
+            {
+                StartActivity(new Intent(this,typeof(Activitys.ActivityTest)));
+            };
+
+        }
+    }
+}
+
